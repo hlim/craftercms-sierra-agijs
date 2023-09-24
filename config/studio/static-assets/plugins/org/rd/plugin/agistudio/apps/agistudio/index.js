7530be073d8dc6180d5f833ae965d28187aad094
russdanner <russ.danner@craftercms.com>
2023-09-23 20:13:29 -0400

    plugin update

--- config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
+++ config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
@@ -867,26 +867,20 @@ function ShowPicture(props) {
                     case 247: // DrawRel
                         var x1 = stream.readUint8();
                         var y1 = stream.readUint8();
-                        // while (true) {
-                        //   var val = stream.readUint8();
-                        //   if (val >= 0xf0) break;
-                        //   var xDisp = (val >>> 4) & 0x07;
-                        //   if ((val & 0x80) === 0x80) xDisp = -xDisp;
-                        //   var yDisp = val & 0x07;
-                        //   if ((val & 8) == 8) yDisp = -yDisp;
-                        //   var nextX = x1 + xDisp;
-                        //   var nextY = y1 + yDisp;
-                        //   x1 = nextX;
-                        //   y1 = nextY;
-                        // }
-                        //this.stream.position--;
                         decodedCommands.push('DrawRel(' + x1 + ',' + y1 + ');');
                         break;
                     case 248: // DrawFill
-                        var x1, y1;
-                        var x1 = stream.readUint8();
-                        var y1 = stream.readUint8();
-                        decodedCommands.push('DrawFill(' + x1 + ',' + y1 + ');');
+                        var args = [];
+                        while (true) {
+                            var x1 = stream.readUint8();
+                            if (x1 >= 0xF0)
+                                break;
+                            args.push(x1);
+                            var y1 = stream.readUint8();
+                            args.push(y1);
+                        }
+                        stream.position--;
+                        decodedCommands.push('DrawFill(' + args.join(",") + ');');
                         break;
                     case 249: // SetPen
                         var value = stream.readUint8();
