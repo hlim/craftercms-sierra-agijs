194dac7487692c33117f315c77395424128d12cf
russdanner <russ.danner@craftercms.com>
2023-09-24 13:58:33 -0400

    plugin update

--- config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
+++ config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
@@ -1066,14 +1066,17 @@ function EditPictureDialog(props) {
     var renderClick = function (event) {
         if (!mouseTrapped) {
             var printMousePosition = function (event) {
-                //alert("Click: "+ event.clientX + ", " + event.clientY)
+                // something is wrong with getting commands from inside this event :-/
                 var x = Math.round(event.clientX / 10);
                 var y = Math.round(event.clientY / 10);
                 alert(commands);
-                var newCommands = commands.replace('End();', '');
+                //@ts-ignore
+                var existingCommands = window.agistudioPicCommands;
+                var newCommands = existingCommands.replace('End();', '');
                 newCommands = newCommands + "DrawAbs(".concat(x, ",").concat(y, ",").concat(x + 5, ",").concat(y + 5, ");\nEnd();");
                 setCommands(newCommands);
-                alert(newCommands);
+                //@ts-ignore
+                window.agistudioPicCommands = newCommands;
                 renderCommands();
             };
             setMouseTrapped(true);
