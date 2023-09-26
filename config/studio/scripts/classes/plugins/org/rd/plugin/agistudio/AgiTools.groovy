17e4e78c0626a369c316e142869faee5d0ac3940
russdanner <russ.danner@craftercms.com>
2023-09-25 21:41:31 -0400

    plugin updates

---
+++ config/studio/scripts/classes/plugins/org/rd/plugin/agistudio/AgiTools.groovy
@@ -0,0 +1,21 @@
+package plugins.org.rd.plugin.agistudio
+
+@Grab(group='io.github.http-builder-ng', module='http-builder-ng-core', version='1.0.4', initClass=false)
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import groovy.json.JsonSlurper
+import groovyx.net.http.HttpBuilder
+
+import static groovyx.net.http.HttpBuilder.configure
+
+/**
+ * 
+ */
+public class AgiTools {
+
+    private static final Logger logger = LoggerFactory.getLogger(AgiToos.class);
+
+   
+   }
