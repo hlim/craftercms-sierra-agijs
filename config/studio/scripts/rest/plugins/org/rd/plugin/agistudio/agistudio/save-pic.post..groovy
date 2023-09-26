a6576b071dce1538619628a71f4a3967d4047af1
russdanner <russ.danner@craftercms.com>
2023-09-25 22:17:03 -0400

    plugin updates

---
+++ config/studio/scripts/rest/plugins/org/rd/plugin/agistudio/agistudio/save-pic.post..groovy
@@ -0,0 +1,18 @@
+@Grapes(
+  @Grab(group='commons-fileupload', module='commons-fileupload', version='1.3.3')
+)
+
+import org.apache.commons.fileupload.disk.DiskFileItemFactory
+import org.apache.commons.fileupload.servlet.ServletFileUpload
+import org.apache.commons.io.FilenameUtils
+
+System.out.println("UPLOAD PIC")
+def factory = new DiskFileItemFactory()
+def upload = new ServletFileUpload(factory)
+def files = upload.parseRequest(request)
+def avatar = files.find { !it.isFormField() }
+def fileName = "blob.${FilenameUtils.getExtension(avatar.getName())}"
+def picStream = picResource.inputStream
+
+
+return picStream
