51feef7f8e2ca9927d510a17afcdebc2f8bb2dd4
russdanner <russ.danner@craftercms.com>
2023-09-28 23:19:22 -0400

    plugin updates

--- config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
+++ config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
@@ -1252,16 +1252,16 @@ function EditPictureDialog(props) {
                 body.append("path", gameContentPath);
                 body.append("name", filename);
                 body.append("type", "application/octet-stream");
+                body.append("relativePath", "null");
+                body.append("validating", "false");
+                body.append("allowed", "true");
+                body.append("suggestedName", "null");
                 body.append("file", new Blob([picsStream]));
                 // body.append("file", new Blob([picsStream]));
                 // site: api-sandbox
-                // relativePath: null
-                // validating: false
                 // path: /static-assets/images
                 // name: vol.0
                 // type: application/octet-stream
-                // allowed: true
-                // suggestedName: null
                 // {
                 //   "Content-Type": "multipart/form-data;",
                 //   "Content-Disposition": "file",
