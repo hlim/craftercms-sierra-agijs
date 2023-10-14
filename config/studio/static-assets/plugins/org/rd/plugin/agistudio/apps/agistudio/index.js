76d6e4bc7b85a1028992b48530f412e3f4c4d364
russdanner <russ.danner@craftercms.com>
2023-10-13 20:33:26 -0400

    plugins

--- config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
+++ config/studio/static-assets/plugins/org/rd/plugin/agistudio/apps/agistudio/index.js
@@ -865,7 +865,8 @@ function ShowCode(props) {
             React.createElement(IconButton, { onClick: handleSaveClick },
                 React.createElement(DataObjectRoundedIcon, null)),
             React.createElement(DialogContent, null,
-                React.createElement(TextField, { id: "outlined-textarea", sx: { width: '100%' }, multiline: true, rows: 20, defaultValue: roomCode }))),
+                React.createElement(TextField, { id: "outlined-textarea", sx: { width: '100%' }, multiline: true, rows: 20, defaultValue: roomCode }),
+                React.createElement(TextField, { id: "outlined-textarea", sx: { width: '100%' }, multiline: true, rows: 20, defaultValue: AgiBridge.decompile(AgiBridge.compile(roomCode)) }))),
         React.createElement(Tooltip, { title: 'Show Code' },
             React.createElement(IconButton, { disabled: !AgiBridge.gameIsLoaded(), size: "medium", style: { padding: 4 }, id: "go-positioned-button", "aria-controls": open ? 'demo-positioned-menu' : undefined, "aria-haspopup": "true", "aria-expanded": open ? 'true' : undefined, onClick: handleClick },
                 React.createElement(DataObjectRoundedIcon, null)))));
